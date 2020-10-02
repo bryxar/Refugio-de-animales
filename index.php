ff7c587739f2c76e658f72516384de371cbe4458
bryxar <72235507+bryxar@users.noreply.github.com>
2020-10-01 18:29:58 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<title>Refugio</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="loans HTML Template">
+	<meta name="keywords" content="loans, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<!-- Favicon -->
+	<link href="images/favicon.ico" rel="shortcut icon"/>
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&display=swap" rel="stylesheet">
+ 
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
+	<link rel="stylesheet" href="css/flaticon.css"/>
+	<link rel="stylesheet" href="css/slicknav.min.css"/>
+
+	<!-- Main Stylesheets -->
+	<link rel="stylesheet" href="css/style.css"/>
+
+
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- Header Section -->
+	<header class="header-section">
+		<a href="index.html" class="site-logo">
+			<img src="images/tema.png"/>
+		</a><br>
+		<nav class="header-nav">
+			<ul class="main-menu">
+				<li><a href="index.php" class="active">Home</a></li>
+				<li><a href="misionyvision.php">Mision y Vision</a></li>
+				<li><a href="objetivos.php">Objetivos</a></li>
+				<li><a href="contactos.php">Contactos</a></li>
+				<li><a href="Login.php">Login</a></li>
+			</ul>
+		</nav>
+	</header>
+	<!-- Header Section end -->
+
+	<!-- Hero Section end -->
+	<section class="hero-section">
+		<div class="hero-slider owl-carousel">
+		    <div class="hs-item set-bg" data-setbg="images/hero-slider/1.jpg"></div>
+			<div class="hs-item set-bg" data-setbg="images/hero-slider/2.jpg"></div>
+			<div class="hs-item set-bg" data-setbg="images/hero-slider/3.jpg"></div>
+			<div class="hs-item set-bg" data-setbg="images/hero-slider/4.jpg"></div>
+			<div class="hs-item set-bg" data-setbg="images/hero-slider/5.jpg"></div>
+			<div class="hs-item set-bg" data-setbg="images/hero-slider/6.jpg"></div>
+		</div>
+	</section>
+	<!-- Hero Section end -->
+
+	<!-- Why Section end -->
+	<section class="why-section spad">
+		<div class="container">
+			<div class="text-center mb-5 pb-4">
+				<h2>Se parte de nuestro proyecto refugio de animales</h2>
+			</div>
+				<div class="ib-text">
+					<h5>Registrarte como una de nuestros usuarios clientes</h5><br><br>
+					
+					<p>"Tendras más privilegios a los cuales podras acceder."</p>
+				</div>
+		</div>
+	</section>
+	<!-- Why Section end -->
+
+	<!-- Help Section -->
+	<section class="help-section spad">
+		<div class="container">
+			<div class="text-center text-white mb-5 pb-4">
+				<h2>Historia del refugio de animales</h2>
+			</div>
+				<div class="ib-text">
+					<p>En El Salvador existen diferentes fundaciones dedicadas a la protección de animales desamparados. Y en la zona oriental de nuestro país nosotros queremos ser una de estas instituciones altruistas que velan por el respeto y la vida de los animales.  Los cuales necesitan de la ayuda de personas de buen corazón que estén dispuestas a donar o a ser voluntarios aportando recursos o tiempo para los animales. Tenemos como propósito el rescate de animales desamparados, así mismo nos dedicamos a la concientización del cuido y respeto de los mismo. Por ello nos encargamos de rehabilitarlos para que luego puedan ser dados en adopción a hogares directos y familias responsables exclusivamente. Somos una institución sin fines de lucro, no poseemos ayuda del gobierno o de ninguna institución privada. La manutención sale de donaciones o de actividades que realizamos como institución para poder generar ingresos. De igual forma todo nuestro equipo de trabajo son personas voluntarias, las cuales ayudan sin cobrar ningún salario. El medio por el cual incentivamos, promovemos y estimulamos a personas a que donen y a que sean voluntarios de nuestra institución es por medio de nuestra página web y de nuestras redes sociales.</p>
+				</div>
+		</div>
+	</section>
+	<!-- Help Section end -->
+
+
+	<!-- Info Section -->
+	<section class="info-section spad">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-5">
+					<img src="images/1.jpg" alt="">
+				</div>
+				<div class="col-lg-7">
+					<div class="info-text">
+						<h2>¿Como puedo ser parte del refugio de animales?</h2>
+						
+						<p>Para ser parte de nosotros tiene que registrarte para formar parte de nuestra familia del refugio, podras acceder a otros privilegios, como ser voluntario para ayudarnos a realizar actividades para la salud de nuestros animalitos, tambien podras acceder a la parte de donación, donde se podra donar ya sea comida o dinero para las mascotas, aunque si quieres estar pendiente de lo que las personas publican en el foro tambien puedes hacerlo, disfruta de la pagina web.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Info Section end -->
+
+	<!-- Footer Section -->
+	<footer class="footer-section">
+		<div class="container">
+			<a href="index.html" class="footer-logo">
+				<img src="images/tema.png" alt="">
+			</a>
+			<p>Esta pagina posee derechos de autor, para no ser plagiado por terceros.</p>
+			<div class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> Irma and Bryan <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Yarique</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></div>
+		</div>
+	</footer>
+	<!-- Footer Section end -->
+	
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.slicknav.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery-ui.min.js"></script>
+	<script src="js/main3.js"></script>
+
+	</body>
+</html>
